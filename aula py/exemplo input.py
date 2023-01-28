28aa76bfbe93de3ef0f5dc1506a235b7fa69193c
ThiagoArica <thiago.arica@mercadolivre.com>
2023-01-28 09:29:46 -0300

    comando input

---
+++ aula py/exemplo input.py
@@ -0,0 +1,8 @@
+
+
+# entrada de dados recebendo com comando input#
+nome = input ("favor digitar o seu nome")
+idade = input("favor digitar a sua idade ")
+print("seu nome é", nome)
+print("sua idade é " , idade )
+
