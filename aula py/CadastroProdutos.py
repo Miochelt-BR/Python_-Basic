cc8f40f11a31145197f5c8d7cc84fc66336078d9
Miochelt-BR <93685091+Miochelt-BR@users.noreply.github.com>
2023-02-14 00:21:27 -0300

    cadastroProdutos

---
+++ aula py/CadastroProdutos.py
@@ -0,0 +1,13 @@
+def produtos_marcado(nome,quantidade,marca):
+    print(f"o nome:{nome}.")
+    print(f"temos {str(quantidade)} dos produtos em estoque ")
+    print(f"a marca: {marca}")
+    
+    
+    
+    
+produtos_marcado("arroz", 2, "camil")
+produtos_marcado("feijao", 2, "camil")
+produtos_marcado("carne", 10, "contra file")
+
+    
