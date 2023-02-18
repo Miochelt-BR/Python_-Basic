dc3e069d0c42f90311d90d88919f2c12b6c699ed
Miochelt-BR <93685091+Miochelt-BR@users.noreply.github.com>
2023-02-18 03:20:54 -0300

    atualizando

---
+++ aula py/_par_impar.py
@@ -0,0 +1,19 @@
+
+print("Dev BY Thiago Arica")
+
+print("=====================================================")
+
+print ("Bem vindo  escolha seu numero ")
+
+print("==================================================")
+try:
+     valor = int(input("digite um valor "))
+     if valor % 2 == 0:
+         print ("numero par ")
+     else:
+        print (" valor igual a  impar")
+except:
+         print ("apenas numeros")
+         
+         
+      
